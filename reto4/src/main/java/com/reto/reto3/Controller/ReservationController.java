d33e06aea3448d3aa7ed5c4d3d98aadc2d3a2270
Ermes <ercast5002@gmail.com>
2022-10-16 21:15:06 -0500

    Actualizaci

---
+++ reto4/src/main/java/com/reto/reto3/Controller/ReservationController.java
@@ -0,0 +1,63 @@
+package com.reto.reto3.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reto.reto3.Service.ReservationService;
+import com.reto.reto3.model.Reservation;
+
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+        RequestMethod.DELETE })
+
+@RestController
+@RequestMapping("/api/Reservation")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping("/all")
+    public List<Reservation> getAll() {
+        return reservationService.getAll();
+
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Reservation> getIdReservation(@PathVariable("id") int id) {
+        return reservationService.getIdReservation(id);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation save(@RequestBody Reservation reservation) {
+        return reservationService.save(reservation);
+
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation update(@RequestBody Reservation reservation) {
+        return reservationService.update(reservation);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int reservationId) {
+        return reservationService.delete(reservationId);
+    }
+
+}
